b34cb1eebef7a46291c22d549cf735ce3cd0406a
Parker <p.cronin.connolly@gmail.com>
2024-02-22 14:22:48 -0500

    7 tests passing

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+    items = []
+    def __init__(self, discount=0, total=0):
+
+        self.discount = discount
+        self.total = total
+
+    def add_item(self, title, price, quantity=1):
+        
+        # print("Adding item")
+        self.title = title
+        self.price = price * quantity
+        self.total = self.price + self.total
+    
+    def apply_discount(self):
+        
+        # print("Applying discount")
+        self.total = self.total - (self.total * (self.discount/100))
+
+
+cash_register = CashRegister(.2)
+print(cash_register.discount)
